8b4bfdde5d815c8a7cedb3ea7aa93e1195952d19
efron <eahmadifar@outlook.com>
2024-03-27 21:17:15 +1100

    Update Dockerfile to use port 3001 instead of 3000 Update .gitignore to ignore .next and out directories Add ms-playwright.playwright extension to VS Code recommendations Add @nx/next and @nx/playwright plugins to nx.json Update main.ts to add /pokemon and /search routes Update package.json to add next, react, react-dom, and @playwright/test dependencies

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,27 @@
 import express from 'express';
-
+import { pokemon } from './pokemon';
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 
 const app = express();
-
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
-
+app.get('/pokemon', (_, res) => {
+  res.send(pokemon);
+});
+app.get('/search', (req, res) => {
+  const q = ((req.query.q as string) ?? '').toLocaleLowerCase();
+  if (!q) {
+    res.status(400).send({ message: 'Missing query parameter `q`' });
+    return;
+  }
+  res.send(
+    pokemon.filter(({ name: { english } }) =>
+      english.toLocaleLowerCase().includes(q)
+    )
+  );
+});
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
